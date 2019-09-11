7133ccedfc3e76c54335b2a8a9e31d4f31d8d6dd
LavaPower <lavapower84@gmail.com>
2019-09-11 21:35:53 +0200

    Add Tkinter, Ttk and Tcl version in Informations

--- pycreator_tkinter/Core/Windows/Informations.py
+++ pycreator_tkinter/Core/Windows/Informations.py
@@ -1,5 +1,5 @@
-from tkinter import Toplevel
-from tkinter.ttk import Label
+from tkinter import Toplevel, TkVersion, TclVersion
+from tkinter.ttk import Label, __version__
 
 import pycreator_tkinter
 import pycreator_core
@@ -14,10 +14,16 @@ class Informations(Toplevel):
         author = Label(self, text="Auteur : LavaPower")
         version = Label(self, text="Version PyCreator-Tkinter : " + pycreator_tkinter.__version__)
         versioncore = Label(self, text="Version PyCreator-Core : " + pycreator_core.__version__)
+        versiontkinter = Label(self, text="Version Tkinter : " + str(TkVersion))
+        versionttk = Label(self, text="Version Ttk : " + __version__)
+        versiontcl = Label(self, text="Version Tcl : " + str(TclVersion))
 
         title.pack(padx=10, pady=10)
         author.pack(padx=10, pady=5)
         version.pack(padx=10, pady=5)
         versioncore.pack(padx=10, pady=5)
+        versiontkinter.pack(padx=10, pady=5)
+        versionttk.pack(padx=10, pady=5)
+        versiontcl.pack(padx=10, pady=5)
 
         self.title("PyCreator - Informations")
